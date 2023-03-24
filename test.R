208b9a6a8a3e52f1206819378e3450937b607406
yuhaihan <yuhaihan0513@mail.ustc.edu.cn>
2023-03-24 10:52:41 +0800

    delete

--- test.R
+++
@@ -1,6 +0,0 @@
-data(doubs,package = "ade4")
-species = doubs$fish
-spa  <- doubs$xy
-plot(spa,asp = 1,type = "n" ,xlab = "x (km)",ylab = "y (km)")
-lines(spa,col = "lightblue")
-text(spa,row.names(spa),cex = 0.5, col = "red")
